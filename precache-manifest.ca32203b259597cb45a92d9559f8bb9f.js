fd5b0d3ad3cecbf6e797861fcf62fbb8ffb3525a
ahujaat2 <39559344+ahujaat2@users.noreply.github.com>
2020-09-16 02:37:45 +0530

    Updates

---
+++ precache-manifest.ca32203b259597cb45a92d9559f8bb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30dfa21bfc320174019ca5aed3863d64",
+    "url": "/simdb/index.html"
+  },
+  {
+    "revision": "5656c9e42e153734044d",
+    "url": "/simdb/static/css/main.13bcab2e.chunk.css"
+  },
+  {
+    "revision": "eee0d13986e9e4da6c96",
+    "url": "/simdb/static/js/2.d6a5a7b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simdb/static/js/2.d6a5a7b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5656c9e42e153734044d",
+    "url": "/simdb/static/js/main.d2b0f552.chunk.js"
+  },
+  {
+    "revision": "1b886bc76a23123c756d",
+    "url": "/simdb/static/js/runtime-main.86059303.js"
+  }
+]);
